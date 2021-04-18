a4b5dda625f825eebf170a3f15f786dd49ed2ef8
prratek <prratek.95@gmail.com>
2021-04-18 00:28:11 -0400

    stub out some test ideas for stream schemas

---
+++ tap_stripe/tests/test_schemas.py
@@ -0,0 +1,13 @@
+"""Tests loading and validity of stream schemas"""
+
+from tap_stripe.tap import TapStripe
+
+
+def test_schemas_load():
+    """Test that all schema files load and parse successfully"""
+    pass
+
+
+def test_fields_have_explicit_types():
+    """Test that all fields have fully specified types - typically required for warehouse targets"""
+    pass
